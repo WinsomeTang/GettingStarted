72f92fcc75cbfc386bb21f916114a470dab01211
Winsome Tang <tangwins@sheridancollege.ca>
2024-03-17 20:48:25 -0400

    Initial Commit

---
+++ GettingStarted/GettingStartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedApp.swift
+//  GettingStarted
+//
+//  Created by Winsome Tang on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
